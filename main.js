e1162c1faed37b2a3e38f492c7e13f6f5568c6c9
raghavsfit <38857049+raghavsfit@users.noreply.github.com>
2019-03-04 00:45:57 +0530

    Prototype 1

---
+++ main.js
@@ -0,0 +1,12 @@
+import {MDCTopAppBar} from '@material/top-app-bar/index';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+import {MDCRipple} from '@material/ripple';
+
+const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
+});
